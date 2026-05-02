1396d0638844b791f571ba305078bd234475cfe6
sanskruthiya <party071985@hotmail.com>
2026-05-02 10:07:53 +0900

    fix: switch to adapter-static for Cloudflare Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,15 +8,10 @@ const config = {
 	},
 	kit: {
 		adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		}),
-		paths: {
-			base: '',
-			assets: ''
-		}
+			pages: 'build',
+			assets: 'build',
+			fallback: '200.html'
+		})
 	}
 };
 
