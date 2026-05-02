9d55a8226a46c36f2cccd4d6da15464b3f8a785f
sanskruthiya <party071985@hotmail.com>
2026-05-02 15:28:04 +0900

    fix: revert to adapter-cloudflare, remove static adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,11 +7,7 @@ const config = {
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '200.html'
-		})
+		adapter: adapter()
 	}
 };
 
